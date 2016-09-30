081e047800804114492800b2c907e73a3d749552
Sowndharya13 <sowndharyakanakaraj@gmail.com>
2016-09-30 12:16:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,7 +14,9 @@ app.get('/myfile1', function (req, res) {
 app.get('/article1', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article1.html'));
 });
-
+app.get('/butter.html', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'butter.html'));
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
