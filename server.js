96b1e46714c38977306d3080a79fcb3515953ee1
Sowndharya13 <sowndharyakanakaraj@gmail.com>
2016-11-25 12:30:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+
+var config={
+    user: 'sowndharya13',
+    database: 'sowndharya13',
+    host: 'db.imad.hasura-app.io',
+    port: '5432',
+    password: process.env.DB_PASSWORD
+};
 
 var app = express();
 app.use(morgan('combined'));
 
+  
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'ggpp1.html'));
@@ -64,6 +74,18 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
+
+var pool= new Pool(config);
+app.get('/test-db', function (req,res)){
+    pool.query('SELECT * FROM test', function(err,result)){
+        if(err){
+            res.status(500).send(err.toString());
+               }
+        else{
+            res.send(JSON.stringify(result));
+            }
+        }
+    }
 
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
